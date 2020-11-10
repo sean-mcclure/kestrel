d7971a79acfa6e9440cebd567a2804f8e5d7736a
Sean McClure <seanaaron100@gmail.com>
2020-11-10 05:58:07 +0000

    adding loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import logo from "./logo.png";
 import {load_messages} from './load_messages.js';
+import {pull_refresh} from './pull_refresh.js';
 
 
 function App() {
   return (
-    <div className="App">
+         <>
+         <div class="spinner">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+        </div>
+        <div className="App">
         <div className="flex_grid_banner">
         <div className="header_col"><img src={logo} className="logo" alt="logo" onClick={load_messages}/></div>
         <div className="header_col"><input className="search" placeholder="search..."></input></div>
@@ -29,6 +36,7 @@ function App() {
         </div>
       
     </div>
+    </>
   );
 }
 
