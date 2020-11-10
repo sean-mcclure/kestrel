1a2501befb94d364411af3f86f16bd53e89d75a5
Sean McClure <seanaaron100@gmail.com>
2020-11-10 03:36:40 +0000

    adding dummy pics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import logo from "./logo.png";
+import {load_messages} from './load_messages.js';
 
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="logo" alt="logo" />
+        <div className="flex_grid_banner">
+        <div className="header_col"><img src={logo} className="logo" alt="logo" onClick={load_messages}/></div>
+        <div className="header_col"><input className="search" placeholder="search..."></input></div>
+        </div>
 
         <div className="flex-grid">
         <div className="col">
@@ -14,10 +18,6 @@ function App() {
             <p>This is some stuff!!!</p>
         </div>
         <div className="col">
-            <p>This is some stuff!!!</p>
-            <p>This is some stuff!!!</p>
-            <p>This is some stuff!!!</p>
-            <p>This is some stuff!!!</p>
         </div>
         <div className="col">
             <p>This is some stuff!!!</p>
---
+++ src/load_messages.js
@@ -0,0 +1,32 @@
+export function load_messages(e) {
+    
+    const messages = [
+        "This is some text that represents a message in Kestrel.",
+        "This is some text that represents a message in Kestrel.",
+        "This is some text that represents a message in Kestrel.",
+        "This is some text that represents a message in Kestrel.",
+        "This is some text that represents a message in Kestrel.",
+        "This is some text that represents a message in Kestrel.",
+        "This is some text that represents a message in Kestrel."
+    ]
+    
+    messages.forEach(function(msg) {
+        var new_div_elem = document.createElement("div");
+        new_div_elem.style.width = "95%"
+        new_div_elem.style.height = "auto"
+        new_div_elem.style.padding = "5px"
+        new_div_elem.style.background = "transparent"
+        new_div_elem.style.marginBottom = "10px"
+        new_div_elem.style.color = "#141414"
+        new_div_elem.style.fontSize = "18px"
+        new_div_elem.style.margin = "10px"
+        new_div_elem.style.borderRadius = "4px"
+        new_div_elem.style.textAlign = "left"
+        new_div_elem.innerHTML = msg
+        var new_img = document.createElement("img");
+        new_img.src = "https://i.redd.it/tk46u5nrkxm21.png"
+        new_img.style.width = "100%"
+        document.getElementsByClassName("col")[1].append(new_div_elem)
+        document.getElementsByClassName("col")[1].append(new_img)
+    })
+}
