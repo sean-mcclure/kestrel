74462abeb9bb36c8df6c86cf424bebcb96335fce
Sean McClure <seanaaron100@gmail.com>
2020-11-19 23:44:38 +0000

    adding multiple images

--- src/Modal.js
+++ src/Modal.js
@@ -1,22 +1,56 @@
 import {character_counter} from "./counter.js";
 import {post} from "./post.js";
-import {register_photo_upload} from "./photo.js"
 import {visible} from "./visible.js"
 
 
 export function Modal() {
-    
+
+    const input_styles = {
+        display : "none"
+    }
+
+    const img_styles = {
+        width : "40%"
+    }
+
     return (
          <>
          <div id="modal" className="modal">
              <div className="show_count">280</div>
-             <div className="close_modal" onClick={(event) => {event.preventDefault(); visible("modal"); document.getElementsByClassName("flex_grid_banner")[0].style.zIndex = 999999999999}}>X</div>
+             <div className="close_modal" onClick={(event) => {event.preventDefault(); visible("modal"); document.getElementsByClassName("flex_grid_banner")[0].style.zIndex = 999999999999; document.getElementsByClassName("hold_uploaded_img")[0].src = ""}}>X</div>
              <textarea id="textarea" className="textarea" onChange={character_counter}></textarea>
-             <div id="hold_uploaded_img" className="hold_uploaded_img"></div>
+             <img id="hold_uploaded_img" className="hold_uploaded_img" alt="uploaded_img_preview" style={img_styles}></img>
              <table className="table">
                  <tbody>
                  <tr>
-                     <td><div className="gif"><input className="upload_input" type="file"></input><button className="upload_image" onClick={register_photo_upload}>UPLOAD</button></div></td>
+                     <td>
+                     <div className="gif">
+                     <input id="upload_input" className="upload_input" type="file" style={input_styles}></input>
+                     <button className="upload_image" onClick={(e) => {
+                            function handle_img(event) {
+                                var file_types = ["png", "jpg", "jpeg", "gif"]
+                                var extension = event.target.files[0].name.split(".")[1]
+                                const is_success = file_types.indexOf(extension) > -1
+                                if (is_success) {
+                                    var reader = new FileReader();
+                                    reader.readAsDataURL(event.target.files[0])
+                                    reader.onload = function(event) {
+                                        if (extension === "png" || extension === "jpg" || extension === "jpeg" || extension === "gif") {
+                                            var file_upload_data_f678sdfa = event.target.result;
+                                            document.getElementsByClassName("upload_input")[0].value = ""
+                                            document.getElementsByClassName("hold_uploaded_img")[0].src = file_upload_data_f678sdfa
+                                        } else {
+                                            alert("Wrong extension. Only JPG, JPEG and GIF accepted.")
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            document.getElementsByClassName("upload_input")[0].click()
+                            document.getElementsByClassName("upload_input")[0].removeEventListener("change", handle_img)
+                            document.getElementsByClassName("upload_input")[0].addEventListener("change", handle_img)
+                            
+                     }}>UPLOAD</button></div></td>
                      <td><div className="gif">GIF</div></td>
                      <td><div className="poll">POLL</div></td>
                      <td><div className="post" onClick={post}>POST</div></td>
--- src/photo.js
+++ src/photo.js
@@ -1,22 +1,20 @@
 export function register_photo_upload() {
-document.getElementsByClassName("upload_input")[0].style.display = "none"
-document.getElementsByClassName("upload_image")[0].addEventListener("click", function() {
-    document.getElementsByClassName("upload_input")[0].click()
-})
-document.getElementsByClassName("upload_input")[0].addEventListener("change", function(event) {
-    var file_types = ["png", "jpg", "jpeg", "gif"]
-    var extension = event.target.files[0].name.split(".")[1]
-    const is_success = file_types.indexOf(extension) > -1
-    if (is_success) {
-        var reader = new FileReader();
-        reader.readAsDataURL(event.target.files[0])
-        reader.onload = function(event) {
-           if (extension === "png" || extension === "jpg" || extension === "jpeg" || extension === "gif") {
-               var file_upload_data_f678sdfa = event.target.result;
-               alert(file_upload_data_f678sdfa)
-           }             
-        }
-    }
-})
+        document.getElementsByClassName("upload_image")[0].addEventListener("click", function() {
+            document.getElementsByClassName("upload_input")[0].click()
+        })
+        document.getElementsByClassName("upload_input")[0].addEventListener("change", function(event) {
+            var file_types = ["png", "jpg", "jpeg", "gif"]
+            var extension = event.target.files[0].name.split(".")[1]
+            const is_success = file_types.indexOf(extension) > -1
+            if (is_success) {
+                var reader = new FileReader();
+                reader.readAsDataURL(event.target.files[0])
+                reader.onload = function(event) {
+                    if (extension === "png" || extension === "jpg" || extension === "jpeg" || extension === "gif") {
+                        var file_upload_data_f678sdfa = event.target.result;
+                        alert(file_upload_data_f678sdfa)
+                    }
+                }
+            }
+        })
 }
-                                                                  
