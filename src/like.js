a43ecbca3fddda3f61b1925aab138bca617c1fa6
Sean McClure <seanaaron100@gmail.com>
2020-11-28 18:24:34 +0000

    working on likes counting

--- src/like.js
+++ src/like.js
@@ -1,12 +1,28 @@
-var like_cnt = 0
-export function like() {
-    like_cnt++
-    var current_value = Number(document.getElementsByClassName("like_count")[0].innerHTML)
-    if((like_cnt%2)===0) {
-        document.getElementsByClassName("like_count")[0].innerHTML = current_value - 1;
-        document.getElementsByClassName("icons")[2].style.color = "whitesmoke";
-    } else {
-        document.getElementsByClassName("like_count")[0].innerHTML = current_value + 1;
-        document.getElementsByClassName("icons")[2].style.color = "#F3B32B";
+var likes = {}
+
+function get_class_instance(classname, id) {
+    const elems = document.getElementsByClassName(classname);
+    for (var i = 0; i < elems.length; i++) {
+        if(elems[i].id === id) {
+            var instance = i
+        }
     }
+    return(instance)
+}
+
+export function like(event) {
+    const id = event.currentTarget.id
+    const class_instance = get_class_instance("like_icons", id)
+    var current_value = Number(document.getElementsByClassName("like_count")[class_instance].innerHTML)
+    likes[id] = current_value
+    if((current_value%2)===0 || typeof(current_value%2)==='undefined') {
+        likes[id] = current_value + 1;
+        document.getElementsByClassName("like_count")[class_instance].innerHTML = likes[id];
+        document.getElementById(id).style.color = "#F3B32B";
+    } else {
+        likes[id] = current_value - 1;
+        document.getElementsByClassName("like_count")[class_instance].innerHTML = likes[id];
+        document.getElementById(id).style.color = "#3D3D3D";
+    }
+
 }
